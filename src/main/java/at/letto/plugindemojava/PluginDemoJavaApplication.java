28664f8ac190db856f8e5543d97544661dd9e637
Werner Damboeck <werner.damboeck@letto.at>
2025-12-19 11:00:56 +0100

    Initial commit

---
+++ src/main/java/at/letto/plugindemojava/PluginDemoJavaApplication.java
@@ -0,0 +1,13 @@
+package at.letto.plugindemojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluginDemoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluginDemoJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/letto/plugindemojava/PluginDemoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package at.letto.plugindemojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluginDemoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
