d96850696e03f051ab4f7c0ca8db0d87868aacb7
Werner Damboeck <werner.damboeck@letto.at>
2025-12-19 11:08:44 +0100

    Base

---
+++ README.md
@@ -0,0 +1,6 @@
+# Plugin Demoprojekt in Java
+
+## github:
+https://github.com/LeTTo-GmbH/plugin-demo-java.git
+
+
--- src/main/java/at/letto/plugindemojava/PluginDemoJavaApplication.java
+++ src/main/java/at/letto/plugindemojava/PluginDemoJavaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PluginDemoJavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PluginDemoJavaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PluginDemoJavaApplication.class, args);
+    }
 
 }
